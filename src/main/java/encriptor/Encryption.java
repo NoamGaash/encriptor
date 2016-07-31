10e207157cb37dfe91f2650d5d5862757b1ba5ce
NoamGaash <noam.gaash@gmail.com>
2016-07-31 18:07:51 +0300

    add dummy encryption / decryption module

---
+++ src/main/java/encriptor/Encryption.java
@@ -0,0 +1,10 @@
+package encriptor;
+
+public class Encryption {
+	public static void encrypt() {
+		System.out.println("pretend to do encryption");
+	}
+	public static void decrypt() {
+		System.out.println("pretend to do decryption");
+	}
+}
